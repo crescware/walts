4bf3533420989f6e1852d06ada15cef227596716
OKUNOKENTARO <okunokentaro@me.com>
2016-07-26 14:01:08 +0900

    fix .d.ts

--- src/action.d.ts
+++ src/action.d.ts
@@ -3,6 +3,9 @@ import { State } from './store';
 export declare type Reducer<ST extends State> = (state: ST) => Promise<ST>;
 export declare type AsyncReducer<ST extends State> = (p: Promise<ST>) => Promise<ST>;
 
+interface RecursiveArray<T> extends Array<T | RecursiveArray<T>> {}
+
 export declare class Action<ST extends State> {
   protected merge(curr: ST, next: ST): ST;
+  protected combine(...reducers: Array<Reducer<ST> | RecursiveArray<Reducer<ST>>>): Reducer<ST>[];
 }
--- src/dispatcher.d.ts
+++ src/dispatcher.d.ts
@@ -2,7 +2,7 @@ import { Reducer, AsyncReducer } from './action';
 import { State } from './store';
 
 export declare class Dispatcher<ST extends State> {
-  emit(reducer: Reducer<ST>): void;
+  emit(reducer: Reducer<ST> | Reducer<ST>[]): void;
   emitAll(reducers: Reducer<ST>[]): void;
   subscribe(observer: (asyncReducer: AsyncReducer<ST>) => void): void;
 }
