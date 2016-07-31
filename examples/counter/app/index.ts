994afe1c6eba4e0dc608eba0741b0d4df2ee02de
OKUNOKENTARO <okunokentaro@me.com>
2016-07-31 22:14:21 +0900

    Example (#4)
    * create base of example
    
    * copy-walts
    
    * implement example

---
+++ examples/counter/app/index.ts
@@ -0,0 +1,8 @@
+import 'core-js';
+import 'rxjs/Rx';
+import 'zone.js/dist/zone';
+
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent, []);
