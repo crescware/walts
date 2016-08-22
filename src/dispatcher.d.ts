a51d2d7091d482d6d48fc00fc2115ce71b45decd
OKUNOKENTARO <okunokentaro@me.com>
2016-08-22 23:13:28 +0900

    fix dispatcher.d.ts

--- src/dispatcher.d.ts
+++ src/dispatcher.d.ts
@@ -1,8 +1,8 @@
-import { MixedAction, Processor } from './actions';
+import { Action, Processor } from './actions';
 import { State } from './store';
 
 export declare class Dispatcher<ST extends State> {
-  emit(action: MixedAction<ST> | MixedAction<ST>[]): void;
-  emitAll(actions: MixedAction<ST>[]): void;
+  emit(action: Action<ST> | Action<ST>[]): void;
+  emitAll(actions: Action<ST>[]): void;
   subscribe(observer: (processor: Processor<ST>) => void): void;
 }
