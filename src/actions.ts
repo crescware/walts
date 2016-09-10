5bd3dd00318213cd01b3aedf375602c4dd7d0405
OKUNOKENTARO <okunokentaro@me.com>
2016-09-11 01:10:25 +0900

    carve out flatten to utils

--- src/actions.ts
+++ src/actions.ts
@@ -1,23 +1,16 @@
 import { State } from './store';
+import { flatten } from './utils';
 
 export type SyncAction <ST extends State> = (state: ST) => ST
 export type AsyncAction<ST extends State> = Promise<(state: ST) => ST>;
 export type Action     <ST extends State> = SyncAction<ST> | AsyncAction<ST>;
 export type Processor  <ST extends State> = (p: Promise<ST>) => Promise<ST>;
 
-interface RecursiveArray<T> extends Array<T | RecursiveArray<T>> {}
+export interface RecursiveArray<T> extends Array<T | RecursiveArray<T>> {}
 
 export class Actions<ST extends State> {
 
   protected combine(...nexts: Array<Action<ST> | RecursiveArray<Action<ST>>>): Action<ST>[] {
-    let flatten = <T>(array: Array<T | RecursiveArray<T>>): Array<T | RecursiveArray<T>> => {
-      return array.reduce<Array<T | RecursiveArray<T>>>((p: Array<T | RecursiveArray<T>>, c: T | RecursiveArray<T>) => {
-        return Array.isArray(c)
-          ? p.concat(flatten<T | RecursiveArray<T>>(c))
-          : p.concat(c);
-      }, []);
-    };
-
     return flatten<Action<ST>>(nexts) as Action<ST>[];
   }
 
